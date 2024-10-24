656b141e5e78f60729df02eae5edb79d0afb310e
Christoffer Sundström <chrsundstrom@gmail.com>
2024-10-24 06:49:55 +0200

    sleepy on bus

--- exercise20/main.c
+++ exercise20/main.c
@@ -1,26 +1,17 @@
-#include <stdbool.h>
-#include <stdint.h>
+#include <ctype.h>
 #include <stdio.h>
 
-int main(void) {
-  const char *s1 = "Roter";
-  char s2[6] = "Roger";
-  char *p = (char *)s1;
-  char **pp = &p;
-
-  *pp = s2;
-  *p = 'P';
-  **pp = 'J';
-  *(*pp + 2) = 'k';
-
-  printf("%s\n", p);
-
-  uint8_t array[4] = {1, 2, 3, 4};
-  bool a = ((void *)array == (void *)&array);
-  bool b = ((void *)(array + 1) == (void *)(&array + 1));
-
-  printf("%s ", a ? "Hello" : "Bye");
-  printf("%s!\n", b ? "Stefan" : "Linda");
-
+int main(void)
+{
+  printf("Enter current time.\nFormat: YYYY-MM-DD HH:mm:ss");
+  char datecheck[20];
+  scanf("%c", datecheck);
+  while ((datecheck == getchar()) != '\n') {
+    if (isalpha(datecheck)) {
+      printf("%s", datecheck);
+    } else if (isspace(datecheck)) {
+      printf("\n");
+    }
+  }
   return 0;
 }
