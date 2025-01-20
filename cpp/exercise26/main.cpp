b756c0c8db4a3a708e9dedfa4108e04afab8456c
Christoffer Sundström <chrsundstrom@mail.com>
2025-01-20 12:04:25 +0100

    jan20

--- cpp/exercise26/main.cpp
+++ cpp/exercise26/main.cpp
@@ -5,8 +5,6 @@ template <typename T> void (*fprint_array)(T[], int) = print_array;
 
 int main() {
   std::array<int, 5> arr{1, 2, 3, 4, 5};
-
   fprint_array<int>(arr.data(), arr.size());
-
   return 0;
 }
